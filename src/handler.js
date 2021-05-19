6d5b6cb47c0bab6034d9bb75fc3b02f028b911ea
OKKY-FIRMANSYAH-RAMADHAN <70622638+OKKY-FIRMANSYAH-RAMADHAN@users.noreply.github.com>
2021-05-19 09:36:18 +0700

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.generateRandomNumber = event => {
   const randomNumber = parseInt(Math.random()*100);
-  console.log("Random number is:".randomNumber);
+  console.log("Random number is:", randomNumber);
   return randomNumber;
 };
